ac80ab928fcdc5762d24587d8b5842c1f4eb0c32
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2017-08-23 17:03:53 +1000

    added a rm with atlist and a cat

--- klpyastro/utils/fileutils.py
+++ klpyastro/utils/fileutils.py
@@ -1,13 +1,68 @@
 import os
 import glob
+import re
 
-def imdelete(inputstring, silent=True):
+def cat(inputstring, outfile=None):
+    if '>' in inputstring:
+        (inputstring, outfile) = inputstring.split('>')
+        inputstring.strip()
+        outfile.strip()
+    else:
+        if outfile is None:
+            raise ValueError
+
+    filelist = atglobparser(inputstring)
+
+    all_lines = []
+    for f in filelist:
+        all_lines.extend(getlines(f))
+
+    all_lines = [line+'\n' for line in all_lines]
+    with open(outfile, 'w') as f:
+        f.writelines(all_lines)
+
+    return
+
+def rm(inputstring, silent=True):
     """
-    Delete .fits files a-la IRAF imdelete.
+    Mimic linux rm, with an atlist too.
+        Allowed inputs:
+            prefix@sci.lis
+            @sci.lis
+            S20001010S0001
+            S20001010S0001.fits
+            S*.fits
+            S20001010S0001.fits, S20001010S0010.fits
+            prefix@sci.lis, @sci.lis, S*.fits, S20001010S0001.fits
 
     Parameters
     ----------
     inputstring : str
+        String to parse.
+
+    silent : bool
+        Whether to warn if a file is not found.  Default is to stay silent.
+
+    Returns
+    -------
+
+    """
+
+    filelist = atglobparser(inputstring)
+
+    for f in filelist:
+        try:
+            os.remove(f)
+        except OSError:
+            if not silent:
+                print('%s not found' % fname)
+
+    return
+
+
+def imdelete(inputstring, silent=True):
+    """
+    Delete .fits files a-la IRAF imdelete.
         Allowed inputs:
             prefix@sci.lis
             @sci.lis
@@ -19,21 +74,18 @@ def imdelete(inputstring, silent=True):
 
         .fits extension: optional, will be added if missing
 
+
+    Parameters
+    ----------
+    inputstring : str
+        String to parse.
+
+    silent : bool
+        Whether to warn if a file is not found.  Default is to stay silent.
+
     """
 
-    # split input string on commas
-    input_elements = [s.strip() for s in inputstring.split(',')]
-
-    filelist = []
-    for element in input_elements:
-        if '@' in element:
-            filelist.extend(getatlist(element))
-        elif '*' in element:
-            filelist.extend(glob.glob(element))
-        elif '?' in element:
-            filelist.extend(glob.glob(element))
-        else:
-            filelist.append(element)
+    filelist = atglobparser(inputstring)
 
     filelist = [s+'.fits' if not s.endswith('.fits') else s for s in filelist]
 
@@ -91,3 +143,43 @@ def getlines(fname):
         content = f.readlines()
     return [line.rstrip() for line in content]
 
+
+def atglobparser(inputstring):
+    """
+    Convert a string into a list of files.
+        Allowed inputs:
+            prefix@sci.lis
+            @sci.lis
+            S20001010S0001
+            S20001010S0001.fits
+            S*.fits
+            S20001010S0001.fits, S20001010S0010.fits
+            prefix@sci.lis, @sci.lis, S*.fits, S20001010S0001.fits
+
+    Parameters
+    ----------
+    inputstring : str
+        The string to parse.
+
+    Returns
+    -------
+    List of filenames
+
+    """
+
+    # split input string on commas
+    #input_elements = [s.strip() for s in inputstring.split(',')]
+    input_elements = list(filter(None, re.split(r'(,|\s)', inputstring)))
+
+    filelist = []
+    for element in input_elements:
+        if '@' in element:
+            filelist.extend(getatlist(element))
+        elif '*' in element:
+            filelist.extend(glob.glob(element))
+        elif '?' in element:
+            filelist.extend(glob.glob(element))
+        else:
+            filelist.append(element)
+
+    return filelist
